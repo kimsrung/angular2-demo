78820c1a7c9b9135659c33d410ad1c589d7e957f
kimsrung <kimsrung.lov@gmail.com>
2017-05-04 15:51:46 +0700

    add form element

---
+++ app/form/form_app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+import {ContactComponent} from './contact_form.component';
+
+@Component({
+	selector: 'my-form',
+	template: '<contact-form></contact-form>',
+	directives: [ContactComponent]
+})
+
+export class FormComponent {
+
+}
---
+++ app/form/form_main.ts
@@ -0,0 +1,4 @@
+import {bootstrap} from 'angular2/platform/browser';
+import {FormComponent} from './form_app.component';
+
+bootstrap(FormComponent);
